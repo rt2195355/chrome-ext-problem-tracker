5addbc5e20df1bbc32c4817055f934e54bf5a201
rt2195355 <rt2195355@gmail.com>
2025-07-20 18:38:43 +0530

    added the functionality to the bookmark button to store the problem details in the sync memory

--- content.js
+++ content.js
@@ -7,6 +7,9 @@
 // License: MIT
 const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png")
 window.addEventListener("load", addBookMarkButton);
+const AZ_PROBLEM_KEY = "AZ_PROBLEM_KEY";
+
+
 
 function addBookMarkButton() {
     const bookMarkButton = document.createElement('img');
@@ -17,9 +20,35 @@ function addBookMarkButton() {
     bookMarkButton.style.cursor = "pointer";
     bookMarkButton.style.marginLeft = '10px';
     const problemTitle = document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0];
-    problemTitle.parentNode.after(bookMarkButton);
-    bookMarkButton.addEventListener('click', () => {
-        alert('Problem bookmarked!');
-    });
+    problemTitle.parentNode.append(bookMarkButton);
+    bookMarkButton.addEventListener('click', addNewBookmarkHandler);
+}
 
+async function addNewBookmarkHandler() {
+    const currentBookmarks = await getCurrentBookmarks();
+    const azProblemURL = window.location.href;
+    const uniqueID = window.location.pathname.split('/')[2]; //using the title of problem as unique id
+    const pt = document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0].textContent;
+
+    if (currentBookmarks.some((b) => b.id === uniqueID)) return; //if already bookmarked
+
+    const bookmarkObj = {
+        id: uniqueID,
+        pname: pt,
+        url: azProblemURL
+    };
+
+    const updatedBookmarks = [...currentBookmarks, bookmarkObj];
+    chrome.storage.sync.set({ AZ_PROBLEM_KEY: updatedBookmarks }, () => {
+        console.log("Updated the bookmarks correctly to ", updatedBookmarks);
+    })
+}
+
+
+function getCurrentBookmarks() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get([AZ_PROBLEM_KEY], (results) => {
+            resolve(results[AZ_PROBLEM_KEY] || []);
+        })
+    });
 }
