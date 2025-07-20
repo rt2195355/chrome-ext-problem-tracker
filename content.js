12a713be3215f55b8b6d479348523023d6d8d323
rt2195355 <rt2195355@gmail.com>
2025-07-20 17:31:28 +0530

    add the bookmark button to the DOM of the webpage

--- content.js
+++ content.js
@@ -5,3 +5,21 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
+const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png")
+window.addEventListener("load", addBookMarkButton);
+
+function addBookMarkButton() {
+    const bookMarkButton = document.createElement('img');
+    bookMarkButton.id = "add-bookmark-button";
+    bookMarkButton.src = bookmarkImgURL;
+    bookMarkButton.style.height = "30px";
+    bookMarkButton.style.width = "30px";
+    bookMarkButton.style.cursor = "pointer";
+    bookMarkButton.style.marginLeft = '10px';
+    const problemTitle = document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0];
+    problemTitle.parentNode.after(bookMarkButton);
+    bookMarkButton.addEventListener('click', () => {
+        alert('Problem bookmarked!');
+    });
+
+}
